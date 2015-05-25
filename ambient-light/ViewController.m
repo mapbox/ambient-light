c1ec75b10b088917c8d62c3634cee0a4546399d0
Jason Wray <jason@kulturny.com>
2015-05-25 00:13:14 -0800

    Add current brightness indicator

--- ambient-light/ViewController.m
+++ ambient-light/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  mbgl-demo-ios
+//  ambient-light
 //
 //  Created by Jason Wray on 5/23/15.
 //  Copyright (c) 2015 Mapbox. All rights reserved.
@@ -16,6 +16,7 @@
 
 @property (nonatomic) IBOutlet UISlider *thresholdSlider;
 @property (nonatomic) CGFloat switchingThreshold;
+@property (nonatomic) CALayer *brightnessIndicator;
 @property (nonatomic) NSURL *lightStyle;
 @property (nonatomic) NSURL *darkStyle;
 
@@ -51,12 +52,13 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [self updateMapStyleForScreenBrightness];
+    [self updateBrightnessIndicator];
      
     // Because Apple doesn't provide any API access to the actual ambient light sensor,
     // we have to use screen brightness as a proxy. Clearly this only works if the user
     // has auto-brightness enabled.
     [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(updateMapStyleForScreenBrightness)
+                                             selector:@selector(brightnessChanged)
                                                  name:UIScreenBrightnessDidChangeNotification
                                                object:nil];
     
@@ -64,6 +66,12 @@
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(updateMapStyleForScreenBrightness)
                                                  name:UIApplicationDidBecomeActiveNotification
+                                               object:nil];
+    
+    // rotate the brightness indicator, too
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(updateBrightnessIndicator)
+                                                 name:UIDeviceOrientationDidChangeNotification
                                                object:nil];
 }
 
@@ -83,6 +91,12 @@
     [self updateMapStyleForScreenBrightness];
 }
 
+- (void)brightnessChanged
+{
+    [self updateMapStyleForScreenBrightness];
+    [self updateBrightnessIndicator];
+}
+
 - (void)updateMapStyleForScreenBrightness
 {
     // map style changes need to happen in the foreground, otherwise the GL view will hang
@@ -95,7 +109,7 @@
             self.mapView.styleURL = self.darkStyle;
             [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent animated:NO];
         }
-        else if (self.switchingThreshold < brightness && ![self.mapView.styleURL isEqual:self.lightStyle])
+        else if (self.switchingThreshold <= brightness && ![self.mapView.styleURL isEqual:self.lightStyle])
         {
             self.mapView.styleURL = self.lightStyle;
             [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault animated:NO];
@@ -103,6 +117,48 @@
         
         // UIViewControllerBasedStatusBarAppearance must be set to NO in Info.plist for status bar styling to work
     }
+}
+
+- (void)updateBrightnessIndicator
+{
+    if (!self.brightnessIndicator)
+    {
+        // setup and place a line to indicate the current brightness
+        self.brightnessIndicator = [CALayer layer];
+        self.brightnessIndicator.bounds = CGRectMake(0, 0, 2, 25);
+        self.brightnessIndicator.position = [self pointOnThresholdSliderForCurrentBrightness];
+        self.brightnessIndicator.cornerRadius = 1;
+        self.brightnessIndicator.shouldRasterize = YES;
+        self.brightnessIndicator.rasterizationScale = [UIScreen mainScreen].scale;
+        self.brightnessIndicator.backgroundColor = [UIView appearance].tintColor.CGColor;
+        
+        // insert in the slider, below its content
+        [self.thresholdSlider.layer insertSublayer:self.brightnessIndicator atIndex:0];
+    }
+    else
+    {
+        // update the brightness indicator position for current brightness
+        CABasicAnimation *moveAnimation = [CABasicAnimation animationWithKeyPath:@"position"];
+        moveAnimation.fromValue = [NSValue valueWithCGPoint:self.brightnessIndicator.position];
+        moveAnimation.removedOnCompletion = NO;
+        moveAnimation.duration = 0.2;
+        moveAnimation.fillMode = kCAFillModeForwards;
+        
+        self.brightnessIndicator.position = [self pointOnThresholdSliderForCurrentBrightness];
+        
+        [self.brightnessIndicator addAnimation:moveAnimation forKey:@"move"];
+    }
+}
+
+- (CGPoint)pointOnThresholdSliderForCurrentBrightness
+{
+    // get horizontal position for indicator, using brightness (min 0, max 1) as a percentage
+    CGFloat x = self.thresholdSlider.layer.bounds.size.width * [[UIScreen mainScreen] brightness];
+    
+    // get the vertical center of the slider, then place the indicator immediately above
+    CGFloat y = (self.thresholdSlider.layer.bounds.size.height / 2) - (self.brightnessIndicator.bounds.size.height / 2);
+    
+    return CGPointMake(x, y);
 }
 
 @end
